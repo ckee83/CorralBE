94c9388606bc3458e48b92ee6c10a879831a0a4f
Alex Bu <alex.bu@lacity.org>
2015-05-27 17:03:49 -0700

    Added weekly schedule entries. Need to work on GUI for each entry to display in blocks.
    Also, need to work on monthly reoccuring schedules.

--- api/controllers/ScheduleController.js
+++ api/controllers/ScheduleController.js
@@ -13,37 +13,37 @@ module.exports = {
 			dayOfWeek: req.param('weekOrMonth')
 		};
 		Schedule.create(sch,function(err, schedule){
-			console.log(schedule);
+			res.redirect('/schedule/index/'+req.params.id);
 		});
-		res.redirect('/schedule/index/'+req.params.id);
 	},
 	index: function(req, res, next){
-		Schedule.find({userID: req.params.id}, function(err, schedules){
+		Schedule.find({userID: req.params.id}).populate('entries').exec(function(err, schedules){
 			if (err)
 				return next(err);
-			for (var schedule in schedules){
-				ScheduleEntry.find({scheduleID: schedule.id}, function(err, entries){
-					for (var i=0; i<entries.length; i++){
-						entries[i].startDT = new Date(entries[i].startDT);
-						entries[i].endDT = new Date(entries[i].endDT);
-						var durationMins = entries[i].endDT-entries[i].startDT;
-						entries[i].duration = durationMins;
-					}
-					entries.sort(function(a,b){
-						if (a.startDT<b.startDT)
-							return -1;
-						else if (a.startDT>b.startDT)
-							return 1;
-						else
-							return 0;
-					});
-					schedule.entries = entries;
+			for (var ind=0; ind<schedules.length; ind++){
+				var entries = schedules[ind].entries;
+				for (var i=0; i<entries.length; i++){
+					entries[i].startDT = new Date(entries[i].startDT);
+					entries[i].endDT = new Date(entries[i].endDT);
+					var durationMins = entries[i].endDT-entries[i].startDT;
+					entries[i].duration = durationMins/60000;
+				}
+				entries.sort(function(a,b){
+					if (a.startDT<b.startDT)
+						return -1;
+					else if (a.startDT>b.startDT)
+						return 1;
+					else
+						return 0;
 				});
-			};
+				schedules[ind].entries=[[],[],[],[],[],[],[]];
+				for (var i=0; i<entries.length; i++)
+					schedules[ind].entries[entries[i].dayOfWeek].push(entries[i]);
+			}
 			res.view({
 				schedules: schedules
 			});
-		})
+		});
 	},
 };
 
--- api/controllers/ScheduleEntryController.js
+++ api/controllers/ScheduleEntryController.js
@@ -6,6 +6,35 @@
  */
 
 module.exports = {
-	
+	create: function(req, res, next){
+		var tempTime = req.param('startTime').split(':');
+		var startDT = new Date();
+		startDT.setHours(tempTime[0]);
+		startDT.setMinutes(tempTime[1]);
+		startDT.setSeconds(0);
+		var endDT = new Date(startDT.getTime() +(req.param('duration')*60000));
+		var entry = {
+			scheduleID: req.param('schID'),
+			description: req.param('description'),
+			dayOfWeek: req.param('dayOfWeek'),
+			startDT: startDT,
+			endDT: endDT
+		}
+		ScheduleEntry.create(entry,function(err, newEntry){
+			if (err)
+				return next(err);
+			if (!newEntry)
+				return next();
+			res.redirect('/schedule/index/'+req.params.id);
+		});
+	},
+	delete: function(req, res, next){
+		console.log(req.param('schID'));
+		ScheduleEntry.destroy({scheduleID: req.param('schID')},function(err){
+			if (err)
+				res.next(err);
+			res.redirect('/schedule/index/'+req.params.id);
+		})
+	}
 };
 
